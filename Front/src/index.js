dafa59ee12efd9700bc8b78a0d259de17928137f
takato <ltakato@hotmail.com>
2020-10-12 12:24:46 -0300

    chore: remove not used bootstrap import on index

--- Front/src/index.js
+++ Front/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
