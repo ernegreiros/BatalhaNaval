6952e0b28e70364b4d20551106bd99d78b50229f
ernegreiros <negreirose48@gmail.com>
2020-10-17 15:10:38 -0300

    Fix some issues in update

--- Api/Battleship/Battleship/Models/Themes/In/InUpdateThemeVM.cs
+++ Api/Battleship/Battleship/Models/Themes/In/InUpdateThemeVM.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace Battleship.Models.Themes.In
 {
-    public class InUpdateThemeVM : InPostThemesVM
+    public class InUpdateThemeVM
     {
         [Required(ErrorMessage ="ID is required")]
         public uint? Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+
     }
 }
