e7500e006a36042f1cb6004674e764afe9f0bc61
ernegreiros <negreirose48@gmail.com>
2020-10-17 14:36:15 -0300

    Added authorization to ships and special power controller

---
+++ Api/Battleship/Battleship/Models/SpecialPower/Out/OutGetSpecialPowerVM.cs
@@ -0,0 +1,9 @@
+﻿using BattleshipApi.Models;
+
+namespace Battleship.Models.SpecialPower.Out
+{
+    public class OutGetSpecialPowerVM : OutBase
+    {
+        public BattleshipApi.SpecialPower.DML.SpecialPower SpecialPower { get; set; }
+    }
+}
