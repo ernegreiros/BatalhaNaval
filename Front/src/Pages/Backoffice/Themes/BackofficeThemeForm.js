5bdf630143db6f2da38cbc00b16561df08755263
takato <ltakato@hotmail.com>
2020-10-24 19:01:22 -0300

    feat: upload de imagem do tema

--- Front/src/Pages/Backoffice/Themes/BackofficeThemeForm.js
+++ Front/src/Pages/Backoffice/Themes/BackofficeThemeForm.js
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import { TextInput, Button } from "react-materialize";
 
+import './BackofficeThemeForm.css';
+
 import ApiClient from "../../../Repositories/ApiClient";
+import {toBase64} from "../../../Utils/fileHelpers";
 
 export function BackofficeThemeForm({ currentTheme = {}, onSaveSuccess }) {
-  const serializeThemeToForm = theme => ({
-    ...theme,
-  });
+  const serializeThemeToForm = theme => ({ ...theme });
   const [theme, setTheme] = useState(currentTheme.id ? serializeThemeToForm(currentTheme) : {})
   const [error, setError] = useState(false)
   const [saving, setSaving] = useState(false)
 
+  async function handleImageChange(event) {
+    const { files } = event.target
+
+    toBase64(files[0])
+      .then(base64 => setTheme({ ...theme, imagePath: base64 }))
+      .catch(() => alert('Falha ao atualizar imagem'))
+  }
+
   function handleSubmit() {
-    const requestData = {
-      ...theme,
-      imagePath: 'teste image path'
-    };
+    const requestData = { ...theme };
 
     setError(false);
     setSaving(true);
@@ -48,6 +54,7 @@ export function BackofficeThemeForm({ currentTheme = {}, onSaveSuccess }) {
     <div className="container">
       <br />
       <form>
+        {theme.imagePath && <img src={theme.imagePath} alt="imagem de capa do tema"/>}
         <TextInput
           label="Nome"
           defaultValue={theme.name}
@@ -58,6 +65,11 @@ export function BackofficeThemeForm({ currentTheme = {}, onSaveSuccess }) {
           defaultValue={theme.description}
           onChange={({ target }) =>
             setTheme({ ...theme, description: target.value })} />
+
+        <TextInput
+          label="Selecione imagem de capa do tema"
+          type="file"
+          onChange={handleImageChange} />
 
         <Button onClick={() => handleSubmit()} disabled={saving || !isValid}>
           {saving ? "Salvando..." : "Salvar"}
