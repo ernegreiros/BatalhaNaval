603407c2c39a8abd3d8dc9aaac7b68e0cbf6d14c
ernegreiros <negreirose48@gmail.com>
2020-11-05 00:32:25 -0300

    Fixed api connection

--- Api/Battleship/Battleship/ClaimsExtensions.cs
+++ Api/Battleship/Battleship/ClaimsExtensions.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace Battleship
 {
@@ -12,8 +10,9 @@ namespace Battleship
         public static string GetJWTUserName(this IEnumerable<Claim> pClaims)
         {
             if (pClaims == null || !pClaims.Any())
-                return string.Empty;            
-            return pClaims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.UniqueName).Value;
+                return string.Empty;
+
+            return pClaims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.UniqueName || c.Properties.Values.FirstOrDefault() == JwtRegisteredClaimNames.UniqueName).Value;
         }
     }
 }
