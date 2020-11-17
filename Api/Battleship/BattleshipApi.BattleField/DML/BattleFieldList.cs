9d05c56a4796ad66e05ac730c276b52476d29119
takato <ltakato@hotmail.com>
2020-11-17 01:01:54 -0300

    fix: alterar para receber battle fields list dentro da propriedade da classe em vez de herdar

--- Api/Battleship/BattleshipApi.BattleField/DML/BattleFieldList.cs
+++ Api/Battleship/BattleshipApi.BattleField/DML/BattleFieldList.cs
@@ -8,19 +8,20 @@ namespace BattleshipApi.BattleField.DML
     /// <summary>
     /// Battle field list
     /// </summary>
-    public class BattleFieldList : List<BattleField>
+    public class BattleFieldList 
     {
+        public List<BattleField> BattleFields { get; set; } = new List<BattleField>();
         /// <summary>
         /// Check datas
         /// </summary>
         public void CheckData()
         {
-            if (!this.Any())
+            if (!BattleFields.Any())
                 throw new Exception("Battle field positions count is zero");
-            else if (this.Count(c => c.PositionObject.X == c.PositionObject.X && c.PositionObject.Y == c.PositionObject.Y) > 1)
-                throw new Exception("Positions duplicate");
+            //else if (BattleFields.Count(c => c.PositionObject.X == c.PositionObject.X && c.PositionObject.Y == c.PositionObject.Y) > 1)
+            //    throw new Exception("Positions duplicate");
             else
-                foreach (BattleField battleField in this)
+                foreach (BattleField battleField in BattleFields)
                 {
                     battleField.CheckData();
                     battleField.PositionObject.CheckData();
